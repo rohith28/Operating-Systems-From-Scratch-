ea465dbfc2fabd40abce6541748257762a7adf1f
rohith28 <rohithuppala28@gmail.com>
2018-01-11 11:24:38 -0500

    Project_A Commit
    This project is to learn more about UNIX commands .  a program that uses multiple processes to compute the sum of a set of (small) positive
    integers.

---
+++ ProjectA/complieOS.sh
@@ -0,0 +1,8 @@
+nasm bootload.asm
+dd if=/dev/zero of=floppya.img bs=512 count=2880
+dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
+bcc -ansi -c -o kernel_c.o kernel.c
+as86 kernel.asm -o kernel_asm.o
+ld86 -o kernel -d kernel_c.o kernel_asm.o
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+java -jar simulator.jar
